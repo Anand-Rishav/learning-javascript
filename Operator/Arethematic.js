25bbf44725252929f0f4bd1d260d230f35ba1134
Rishav Anand <tominati6@gmail.com>
2025-10-18 20:41:54 +0530

    Arethematic operator

---
+++ Operator/Arethematic.js
@@ -0,0 +1,19 @@
+// Operator in Javascript
+// Arethematic operator : These are normal maths operator to solve maths problem 
+// +, -, /, * These are noraml mathematiocal operator under arethematic operator
+const a = 5 ;
+const b = 6;
+console.log(a+b); // This is addition operator
+console.log(a-b); // This is subtraction operator
+console.log(a*b); // This is multiplication operator for this we use astris sign
+console.log(a/b); // This is division operator
+// Now some more arethematic operator : 
+console.log(a%b); // this is modulas operator this is used for giving the remainder 
+console.log(a**b) ;// This is exponention operator this is use to give power like a power b 
+// And now two more : increment and decrement (a++ and a--)
+// It is nothing more it is just like a++ = a+1 and a__ is a-1
+// There are two type of these post incresent and pre incresemt  same with decrement 
+// pre increment is it execute befor and then execute and and post increment is it execute first then increment same with decrement 
+let x = 10;
+console.log(x++);// this is post increment as this will execute first then add later
+console.log(++x); // this is pre increment as this will add first then it execute 
