a798b166957001278518b9860487656d54aa2d58
Rishav Anand <tominati6@gmail.com>
2025-10-18 18:41:40 +0530

    comment JS

---
+++ comment.js
@@ -0,0 +1,12 @@
+// Comment are kind of code which not execute it is there to tell about the code 
+// Like a mmessage 
+console.log('hello world')// this will print hello world 
+// This is for single line 
+// for multiple line we use /**/
+
+/* Hello this is 
+multiple line comment these are use if you want to comment multiple line 
+and the ending is */
+// Now as it wont execute and give us the code result without executing 
+const variable = "Rishav Anand"
+console.log(variable)
